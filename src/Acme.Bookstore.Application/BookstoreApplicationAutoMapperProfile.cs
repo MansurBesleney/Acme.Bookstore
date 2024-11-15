4c87e05f92ffe79b7ef70f5dfdcafd3b3bd4f757
Mansur Besleney <besleneymansur@gmail.com>
2024-11-15 15:57:28 +0300

    service interface and dtos created

--- src/Acme.Bookstore.Application/BookstoreApplicationAutoMapperProfile.cs
+++ src/Acme.Bookstore.Application/BookstoreApplicationAutoMapperProfile.cs
@@ -1,3 +1,4 @@
+using Acme.Bookstore.Books;
 using AutoMapper;
 
 namespace Acme.Bookstore;
@@ -9,5 +10,8 @@ public class BookstoreApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+
+        CreateMap<Book, BookDto>();
+        CreateMap<CreateUpdateBookDto, Book>();
     }
 }

77c61864453bd5b8a4eca0049522b793832494d6
Mansur Besleney <besleneymansur@gmail.com>
2024-11-20 13:46:03 +0300

    integration tests added

---
+++ test/Acme.Bookstore.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Books/EfCoreBookAppService_Tests.cs
@@ -0,0 +1,16 @@
+﻿using Acme.Bookstore.Books;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Acme.Bookstore.EntityFrameworkCore.Applications.Books
+{
+    [Collection(BookstoreTestConsts.CollectionDefinitionName)]
+    public class EfCoreBookAppService_Tests : BookAppService_Tests<BookstoreEntityFrameworkCoreTestModule>
+    {
+
+    }
+}

f618ab8f539aad6605a494d52fd43c97e5046661
Mansur Besleney <besleneymansur@gmail.com>
2024-11-22 16:40:42 +0300

    Author entity added to domain layer

---
+++ src/Acme.Bookstore.Domain/Authors/Author.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.Emailing;
+using Volo.Abp.OpenIddict.Authorizations;
+
+namespace Acme.Bookstore.Authors
+{
+    public class Author : FullAuditedAggregateRoot<Guid>
+    {
+        private string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string ShortBio {  get; set; }
+
+        private Author() { }
+
+        internal Author(
+            Guid id,
+            string name,
+            DateTime birthDate,
+            string shortBio)
+        {
+            SetName(name);
+            BirthDate = birthDate;
+            ShortBio = shortBio;
+        }
+
+        internal Author ChangeName(string name)
+        {
+            SetName(name);
+            return this;
+        }
+
+        private void SetName(string Name)
+        {
+            Name = Check.NotNullOrWhiteSpace(
+                Name,
+                nameof(Name),
+                maxLength : AuthorConsts.MaxNameLength);
+        }
+    }
+}
